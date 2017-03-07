411f5469361d82a642bd53cf4856ac5403f271ce
Majewski189722 <KrzysztofJanMajewski@gmail.com>
2017-03-07 23:55:50 +0100

    tests

---
+++ my-app/src/test/java/com/mycompany/app/TestClass.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestClass {
+
+	@Test
+	public void test() {
+		assertEquals(1,1);
+	}
+
+}
